1a3fa4a7a5dbfbee5f696b914483db275fa227f4
Kuroneko <pablovirues@gmail.com>
2024-02-27 18:55:38 +0100

    tags

--- Assets/Script/JugadorBola.cs
+++ Assets/Script/JugadorBola.cs
@@ -48,6 +48,8 @@ public class JugadorBola : MonoBehaviour
         {
             StartCoroutine(BorrarSuelo(other.gameObject));
         }
+
+        
     }
 
     IEnumerator BorrarSuelo(GameObject suelo)
