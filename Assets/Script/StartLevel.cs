7cdcd88381f3a0983b284893359639f463b76824
Kuroneko <pablovirues@gmail.com>
2024-03-02 12:59:20 +0100

    Varios GUI stuff

--- Assets/Script/StartLevel.cs
+++ Assets/Script/StartLevel.cs
@@ -1,31 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Levels : MonoBehaviour
+public class StartLevel : MonoBehaviour
 {
-
-    AudioManager audioManager;
     // Start is called before the first frame update
     void Start()
     {
-        //detiene la musica del audiomanager
-        AudioManager.instance.Play("Menu");
-
         Button button = GetComponent<Button>();
         button.onClick.AddListener(TaskOnClick);
     }
 
     void TaskOnClick()
     {
+        FindObjectOfType<AudioManager>().Play("StartLevel");
+        FindObjectOfType<AudioManager>().Stop("Menu");
         StartCoroutine(LoadLevel());
     }
 
     IEnumerator LoadLevel()
     {
-        yield return new WaitForSeconds(0.5f);
-        SceneManager.LoadScene("LevelSelector");
+        yield return new WaitForSeconds(1);
+        string level = gameObject.name;
+        PlayerPrefs.SetString("LevelSelected", level[5].ToString());
+        SceneManager.LoadScene(level);
     }
 }
